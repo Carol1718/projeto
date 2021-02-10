4e0474ac1db493e8120e988bc13219f07eb2a245
CAROL MENDES FERREIRA <20272542@cfp127aluno.local>
2021-02-10 11:25:05 -0300

    10-02-2021-parte2

---
+++ vetores2/src/vetores2/Vetor.java
@@ -0,0 +1,30 @@
+package vetores2;
+
+public class Vetor {
+
+	public static void main(String[] args) {
+		double notas[] = {8.5, 9.3, 7.2, 9.7};
+		
+		System.out.println("Média" = media(notas));
+		
+		double idades[] = {46, 17, 17, 16, 17, 17, 17, 16, 17, 22, 17, 19, 16, 16, 17, 16, 16};
+		
+		System.out.println(" Media das Idades = " + media(idades));
+		System.out.println(" Somas das notas = " + soma(notas));
+		
+		double peso[] = {65, 89, 50, 48, 100, 65, 78, 58};
+		System.out.println(" Media dos pesos = " + media(peso));
+		System.out.println(" Total de peso = " + soma(peso));
+		
+		String[] nomes = { }
+				
+		
+ 
+	   
+   }
+    	
+  
+       }
+	
+
+
